ddf0a7eafdf4c84ed71b0e559e8ade7fa2f813fd
tanaymalik <tanaymalik06@gmail.com>
2024-03-09 22:03:10 +0530

    ui added

---
+++ script.js
@@ -0,0 +1,13 @@
+
+const add = (a, b) => a + b;
+
+const subtract = (a, b) => a - b;
+
+const multiply = (a, b) => a * b;
+
+const divide = (a, b) => a / b;
+
+
+
+const operate = (operator, num1, num2) => operator(num1, num2);
+
